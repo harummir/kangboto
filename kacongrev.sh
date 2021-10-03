1ca508e11c7f1d99d4e750c996fdfe48df0b8f9f
harummir <88335789+harummir@users.noreply.github.com>
2021-10-03 10:22:11 -0500

    Update kacongrev.sh

--- kacongrev.sh
+++ kacongrev.sh
@@ -1,1 +1,1 @@
-sudo ./nbminer -a ergo -o stratum+tcp://us.ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_AREA_kacong -log
+sudo ./nbminer -a ergo -o stratum+tcp://us.ergo.herominers.com:1180 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_AREA_kacong -log
